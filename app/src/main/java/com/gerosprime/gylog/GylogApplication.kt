2bb059cd201922a64f56f079f1b0b193e9e185f5
Glenn Ross Pascual <glenn.development@gmail.com>
2019-10-12 11:03:58 +0800

    Added bottom navigation in app module. Implemented dashboard programs in app module.

--- app/src/main/java/com/gerosprime/gylog/GylogApplication.kt
+++ app/src/main/java/com/gerosprime/gylog/GylogApplication.kt
@@ -4,23 +4,28 @@ import android.app.Activity
 import android.app.Application
 import android.app.Service
 import androidx.fragment.app.Fragment
+import com.gerosprime.gylog.components.di.DaggerGylogComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import dagger.android.HasServiceInjector
 import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
 class GylogApplication : Application(), HasActivityInjector,
     HasServiceInjector, HasSupportFragmentInjector {
 
 
+    @Inject
     lateinit var dispatchingInjectorAct : DispatchingAndroidInjector<Activity>
+    @Inject
     lateinit var dispatchingInjectorSer : DispatchingAndroidInjector<Service>
+    @Inject
     lateinit var dispatchingInjectorFra : DispatchingAndroidInjector<Fragment>
 
     override fun onCreate() {
         super.onCreate()
-
+        DaggerGylogComponent.create().inject(this)
     }
 
     override fun activityInjector(): AndroidInjector<Activity> = dispatchingInjectorAct
--- app/src/main/java/com/gerosprime/gylog/components/di/GylogComponent.kt
+++ app/src/main/java/com/gerosprime/gylog/components/di/GylogComponent.kt
@@ -5,10 +5,13 @@ import com.gerosprime.gylog.components.di.modules.FragmentBuilders
 import com.gerosprime.gylog.components.di.modules.ProgramsModelModule
 import com.gerosprime.gylog.components.di.modules.ViewModelModule
 import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Component(modules = [FragmentBuilders::class, ViewModelModule::class,
-    ProgramsModelModule::class])
+    ProgramsModelModule::class,
+    AndroidInjectionModule::class, AndroidSupportInjectionModule::class])
 @Singleton
 interface GylogComponent {
     fun inject(application: GylogApplication)
