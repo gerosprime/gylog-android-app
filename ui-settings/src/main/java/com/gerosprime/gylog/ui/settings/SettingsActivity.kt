a0dfd0f3d3da4152fd058248dbb671de451331fc
Glenn Ross Pascual <glenn.development@gmail.com>
2019-12-07 00:42:30 +0800

    Added about section.

--- ui-settings/src/main/java/com/gerosprime/gylog/ui/settings/SettingsActivity.kt
+++ ui-settings/src/main/java/com/gerosprime/gylog/ui/settings/SettingsActivity.kt
@@ -2,7 +2,9 @@ package com.gerosprime.gylog.ui.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import me.jfenn.attribouter.Attribouter
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -23,6 +25,21 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+
+            when (preference?.key) {
+                "application_developer" -> {
+                    showDeveloper()
+                }
+            }
+
+            return super.onPreferenceTreeClick(preference)
+        }
+
+        private fun showDeveloper() {
+            Attribouter.from(context).withFile(R.xml.about).show()
         }
     }
 }
