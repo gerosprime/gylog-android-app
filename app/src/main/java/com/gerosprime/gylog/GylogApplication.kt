a550138b2b86cd7f4de4c4e3e98f856d84cfe89c
Glenn Ross Pascual <glennpascual011@gmail.com>
2020-06-26 11:32:11 +0800

    Upgraded Dagger to 2.26. Fixed compilation error for post upgrade of Dagger.

--- app/src/main/java/com/gerosprime/gylog/GylogApplication.kt
+++ app/src/main/java/com/gerosprime/gylog/GylogApplication.kt
@@ -1,28 +1,17 @@
 package com.gerosprime.gylog
 
-import android.app.Activity
 import android.app.Application
-import android.app.Service
-import androidx.fragment.app.Fragment
 import com.gerosprime.gylog.components.di.DaggerGylogComponent
 import com.gerosprime.gylog.models.states.ModelsCache
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.HasServiceInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class GylogApplication : Application(), HasActivityInjector,
-    HasServiceInjector, HasSupportFragmentInjector {
-
+class GylogApplication : Application(), HasAndroidInjector {
 
     @Inject
-    lateinit var dispatchingInjectorAct : DispatchingAndroidInjector<Activity>
-    @Inject
-    lateinit var dispatchingInjectorSer : DispatchingAndroidInjector<Service>
-    @Inject
-    lateinit var dispatchingInjectorFra : DispatchingAndroidInjector<Fragment>
+    lateinit var dispatchingInjectorAct : DispatchingAndroidInjector<Any>
 
     @Inject
     lateinit var modelsCache: ModelsCache
@@ -33,11 +22,7 @@ class GylogApplication : Application(), HasActivityInjector,
             .context(this).build().inject(this)
     }
 
-    override fun activityInjector(): AndroidInjector<Activity> = dispatchingInjectorAct
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingInjectorFra
-
-    override fun serviceInjector(): AndroidInjector<Service> = dispatchingInjectorSer
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingInjectorAct
 
     override fun onTerminate() {
         modelsCache.clear()
